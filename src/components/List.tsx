d6acac7cdfdc4ff4cba9b173f007632b01476b2d
mdraselmiah95 <russelroy15@gmail.com>
2022-03-20 01:08:17 +0600

    new package install

---
+++ src/components/List.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const List = ({ items, onClick }: { items: string[]; onClick: () => void }) => {
+  return (
+    <div>
+      <h2>List</h2>
+    </div>
+  );
+};
+
+export default List;
