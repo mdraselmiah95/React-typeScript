ca54c99ad0f8f3bc7e373ef56dd34fab3ec3b3bf
mdraselmiah95 <russelroy15@gmail.com>
2022-03-27 16:44:44 +0600

    new code added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import Lists from "./components/Lists";
 
-const Box: React.FunctionComponent<{ title: string }> = ({ title }) => {
-  return <div></div>;
-};
+// const Box: React.FunctionComponent<{ title: string }> = ({ title }) => {
+//   return <div></div>;
+// };
+interface Todo {
+  id: number;
+  text: string;
+}
+
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
 
 function App() {
+  //useReducer
+  function reducer(state: Todo[], action: ActionType) {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+    }
+  }
+
+  const [todos, dispatch] = useReducer(reducer, []);
+
   return (
     <div>
-      <Box title="hello"></Box>
-      <Lists />
+      {/* <Box title="hello"></Box>
+      <Lists /> */}
+      <h2>h1</h2>
     </div>
   );
 }
