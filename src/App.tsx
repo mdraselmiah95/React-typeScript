29267f12c016d0c612adfd120e27708c123a236e
mdraselmiah95 <russelroy15@gmail.com>
2022-03-27 16:12:04 +0600

    new code added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Lists from "./components/Lists";
+
+const Box: React.FunctionComponent = ({ children }) => {
+  return <div>{children}</div>;
+};
 
 function App() {
   return (
     <div>
-      <h2>Hi this is Rasel.</h2>
+      <Box></Box>
+      <Lists />
     </div>
   );
 }
