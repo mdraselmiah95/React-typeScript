ab553f5ef930bcb87d9b9c140adb1730bdb86eee
mdraselmiah95 <russelroy15@gmail.com>
2022-04-04 05:21:12 +0600

    WIP on TODO project

--- src/components/inputField/SIngleTodo.tsx
+++ src/components/inputField/SIngleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Todo } from "../../model";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -36,13 +36,20 @@ const SIngleTodo = ({ todo, todos, setTodos }: Props) => {
     setEdit(false);
   };
 
+  //   useEffect(() => {
+  //     inputRef.current?.focus();
+  //   }, [edit]);
+
+  //   const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
       {edit ? (
         <input
+          //   ref={inputRef}
           value={editTodo}
           onChange={(e) => setEditTodo(e.target.value)}
-          className="todos__single--test"
+          className="todos__single--text"
         />
       ) : todo.isDone ? (
         <s className="todos__single--text">{todo.todo}</s>
