808edc814e812796b2427cab4ed0b7813cb2fb2a
mdraselmiah95 <russelroy15@gmail.com>
2022-04-04 02:42:34 +0600

    WIP on TODO project

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ const App: React.FC = () => {
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
   };
 
   return (
